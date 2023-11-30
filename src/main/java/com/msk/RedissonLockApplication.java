ede057a07573f42a1759ac7cbfe7bada710a7471
MSK <mashikai706@gmail.com>
2023-11-30 16:04:08 +0800

    init

---
+++ src/main/java/com/msk/RedissonLockApplication.java
@@ -0,0 +1,15 @@
+package com.msk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: MSK
+ * @date: 2023/11/30 15:44
+ */
+@SpringBootApplication
+public class RedissonLockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonLockApplication.class);
+    }
+}
